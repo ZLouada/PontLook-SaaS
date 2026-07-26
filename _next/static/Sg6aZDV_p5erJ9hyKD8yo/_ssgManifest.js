aeb6392858799bbe980ad61dd5c93130cccdc441
ZLouada <zakaria.louada-etu@etu.univh2c.ma>
2026-07-26 04:08:55 +0100

    chore: remove build artifacts and misplaced local directories

--- _next/static/Sg6aZDV_p5erJ9hyKD8yo/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[lang]","\u002F[lang]\u002Fcontact","\u002F[lang]\u002Ffind-training","\u002F[lang]\u002Ffor-providers","\u002F[lang]\u002Fresearch","\u002F[lang]\u002Fresearch\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- _next/static/bmDx2stdIAYCb1k29OFEL/_clientMiddlewareManifest.js
+++
@@ -1,6 +0,0 @@
-self.__MIDDLEWARE_MATCHERS = [
-  {
-    "regexp": "^(?:\\/(_next\\/data\\/[^/]{1,}))?(?:\\/((?!_next|favicon\\.ico|favicon-.*\\.png|apple-touch-icon\\.png|android-chrome-.*\\.png|site\\.webmanifest|sitemap\\.xml|robots\\.txt|PontLook-Logo\\.png|logo-white\\.png|skyline-bg\\.jpg|skyline-bg\\.webp|og-image\\.png|api).*))(\\.json|\\.rsc|\\.segments\\/.+\\.segment\\.rsc)?[\\/#\\?]?$",
-    "originalSource": "/((?!_next|favicon\\.ico|favicon-.*\\.png|apple-touch-icon\\.png|android-chrome-.*\\.png|site\\.webmanifest|sitemap\\.xml|robots\\.txt|PontLook-Logo\\.png|logo-white\\.png|skyline-bg\\.jpg|skyline-bg\\.webp|og-image\\.png|api).*)"
-  }
-];self.__MIDDLEWARE_MATCHERS_CB && self.__MIDDLEWARE_MATCHERS_CB()
